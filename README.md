dd21b61bf6d55216dd075710b74b39651337a307
bijaya-dulal <dulalbijaya0@gmail.com>
2024-07-04 09:44:37 +0545

    regain djangoproject

--- README.md
+++
@@ -1,2 +0,0 @@
-# E-Learning-Platform
- Academic Minor Project
--- djangoproj/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ItemListCreateView,UserCreateView
-
-urlpatterns = [
-    path('items/', ItemListCreateView.as_view(), name='item-list-create'),
-        path('users/', UserCreateView.as_view(), name='user-create'),
-]
